04280be8506630460bd410e3a601415d0ea90fcf
Vinicius Aias <viniciusaias200@gmail.com>
2026-04-22 09:44:47 -0300

    começo da integração com banco de dados

---
+++ smartbiancoweb/src/main/java/com/smartbianco/smartbiancoweb/SmartbiancowebApplication.java
@@ -0,0 +1,13 @@
+package com.smartbianco.smartbiancoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbiancowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbiancowebApplication.class, args);
+	}
+
+}
---
+++ smartbiancoweb/src/test/java/com/smartbianco/smartbiancoweb/SmartbiancowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbianco.smartbiancoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbiancowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
